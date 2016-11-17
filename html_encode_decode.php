dd5ab9dffeae49a5f538e7cbd09b6bf665d42c83
ravikumarmmt <ravikumarmmt@gmail.com>
2016-11-17 15:47:18 +0530

    travis config yml file

--- html_encode_decode.php
+++ html_encode_decode.php
@@ -9,11 +9,11 @@ echo $stringc = html_entity_decode($string)."<br/>";
 
 
 $orig = "I'll \"walk\" the <b>dog</b> now";
-//
+
 $a = htmlentities($orig);
-//
+
 //$b = html_entity_decode($a);
-//
+
 //echo $a; // I'll &quot;walk&quot; the &lt;b&gt;dog&lt;/b&gt; now
 //
 //echo $b; // I'll "walk" the <b>dog</b> now
